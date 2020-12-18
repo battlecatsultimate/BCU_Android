0618a027df7b7bf3d96d9c753080db7b9cbf99cf
MandarinSmell <geawonlee@naver.com>
2020-12-18 20:29:35 +0900

    update google drive code

--- app/src/main/java/com/mandarin/bcu/androidutil/io/drive/DriveUtil.kt
+++ app/src/main/java/com/mandarin/bcu/androidutil/io/drive/DriveUtil.kt
@@ -1,7 +1,6 @@
 package com.mandarin.bcu.androidutil.io.drive
 
 import android.util.Log
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential
 import com.google.api.client.http.FileContent
 import com.google.api.client.http.HttpTransport
 import com.google.api.client.http.javanet.NetHttpTransport
@@ -9,6 +8,8 @@ import com.google.api.client.json.JsonFactory
 import com.google.api.client.json.jackson2.JacksonFactory
 import com.google.api.services.drive.Drive
 import com.google.api.services.drive.DriveScopes
+import com.google.auth.http.HttpCredentialsAdapter
+import com.google.auth.oauth2.GoogleCredentials
 import com.mandarin.bcu.androidutil.StaticStore
 import java.io.File
 import java.io.IOException
@@ -17,10 +18,11 @@ import java.io.InputStream
 object DriveUtil {
     @Throws(IOException::class)
     fun upload(file: File, `in`: InputStream?): Boolean {
-        val credential = GoogleCredential.fromStream(`in`).createScoped(listOf(DriveScopes.DRIVE))
+        val credential = GoogleCredentials.fromStream(`in`).createScoped(listOf(DriveScopes.DRIVE))
+        val request = HttpCredentialsAdapter(credential)
         val transport: HttpTransport = NetHttpTransport()
         val factory: JsonFactory = JacksonFactory.getDefaultInstance()
-        val service = Drive.Builder(transport, factory, credential).setApplicationName("BCU").build()
+        val service = Drive.Builder(transport, factory, request).setApplicationName("BCU").build()
         Log.i("uploadDrive", "-*****- Uploading Log File to Drive -> File Name : " + file.name + " | File Size : " + file.length() + " -*****-")
         val target = com.google.api.services.drive.model.File()
         val con = FileContent("text/txt", file)
