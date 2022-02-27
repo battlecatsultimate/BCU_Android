a1ced193d931e69e6a158569e2255b14c089a4cb
MandarinSmell <mandarinsmellbcu@gmail.com>
2022-02-27 20:13:31 +0900

    0.16.6

--- app/src/main/java/com/mandarin/bcu/androidutil/StaticStore.kt
+++ app/src/main/java/com/mandarin/bcu/androidutil/StaticStore.kt
@@ -72,7 +72,7 @@ import kotlin.math.ln
 object StaticStore {
     //System & IO variables
     /**Version of Application */
-    const val VER = "0.16.5"
+    const val VER = "0.16.6"
 
     /**File ID of google drive log folder */
     const val ERR_FILE_ID = "1F60YLwsJ_zrJOh0IczUuf-Q1QyJftWzK"
