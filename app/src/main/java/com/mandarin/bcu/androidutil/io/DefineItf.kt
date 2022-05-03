b9e789e7f9acce5a373ce5a9bc6efe70298bb789
しき中村 <65457534+Blacksun420@users.noreply.github.com>
2022-05-02 23:55:33 -0400

    Remove deprecated & unused code

--- app/src/main/java/com/mandarin/bcu/androidutil/io/DefineItf.kt
+++ app/src/main/java/com/mandarin/bcu/androidutil/io/DefineItf.kt
@@ -7,43 +7,20 @@ import android.media.MediaMetadataRetriever
 import com.mandarin.bcu.androidutil.StaticStore
 import com.mandarin.bcu.androidutil.battle.sound.SoundHandler
 import com.mandarin.bcu.androidutil.music.OggDataSource
-import com.mandarin.bcu.androidutil.pack.AACLoader
-import com.mandarin.bcu.androidutil.pack.AImageReader
-import com.mandarin.bcu.androidutil.pack.AMusicLoader
 import common.CommonStatic
 import common.CommonStatic.Itf
-import common.io.InStream
 import common.pack.Identifier
-import common.pack.Source
 import common.util.stage.Music
 import java.io.File
-import java.util.*
 
 class DefineItf : Itf {
     companion object {
         var dir: String = ""
-        val packPath = ArrayList<String>()
 
         fun check(c: Context) {
             if(dir == "") {
                 dir = StaticStore.getExternalPath(c)
             }
-        }
-    }
-
-    override fun loadAnim(ins: InStream?, r: CommonStatic.ImgReader?): Source.AnimLoader {
-        var name = ""
-
-        return if(r != null) {
-            name = (r as AImageReader).name
-
-            if(r.isNull) {
-                AACLoader(ins, dir, name)
-            } else {
-                AACLoader(ins, r)
-            }
-        } else {
-            AACLoader(ins, dir, name)
         }
     }
 
@@ -58,10 +35,7 @@ class DefineItf : Itf {
         return mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)?.toLong() ?: -1
     }
 
-    override fun readBytes(fi: File): InStream? {
-        return InStream.getIns(fi)
-    }
-
+    @Deprecated("Deprecated in Java")
     override fun route(path: String?): File {
         val realPath = path?.replace("./",dir) ?: ""
 
@@ -74,31 +48,6 @@ class DefineItf : Itf {
 
     override fun setBGM(mus: Identifier<Music>) {
         SoundHandler.setBGM(mus)
-    }
-
-    override fun getMusicReader(pid: Int, mid: Int): CommonStatic.ImgReader {
-        return AMusicLoader(pid, mid)
-    }
-
-    override fun getReader(f: File?): CommonStatic.ImgReader? {
-        val path = f?.absolutePath ?: ""
-
-        return when {
-
-            path.endsWith(".bcupack") -> {
-                packPath.add(f?.absolutePath ?: "")
-                AImageReader(f?.name?.replace(".bcupack", "")?.replace(".bcudata", "")
-                        ?: "", true)
-            }
-
-            path.endsWith(".bcudata") -> {
-                AImageReader(f?.name?.replace(".bcupack", "")?.replace(".bcudata", "")
-                        ?: "", false)
-            }
-            else -> {
-                null
-            }
-        }
     }
 
     fun init(c: Context) {
