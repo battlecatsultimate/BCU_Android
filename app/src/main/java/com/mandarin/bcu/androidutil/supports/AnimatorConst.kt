91fbdbfea0bd1dcdd7a67b568a9f578a153b92f0
MandarinSmell <49793724+MandarinSmell@users.noreply.github.com>
2023-08-03 11:09:47 +0900

    use enum instead of constant value

--- app/src/main/java/com/mandarin/bcu/androidutil/supports/AnimatorConst.kt
+++ app/src/main/java/com/mandarin/bcu/androidutil/supports/AnimatorConst.kt
@@ -17,12 +17,4 @@ object AnimatorConst {
         X,
         Y
     }
-
-    const val HEIGHT = 0
-    const val WIDTH = 1
-    const val TOP_MARGIN = 2
-
-    const val DECELERATE = 0
-    const val ACCELERATE = 1
-    const val ACCELDECEL = 2
 }
